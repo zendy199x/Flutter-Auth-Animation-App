bafc25c9c67ada917d188ad0a1bcfa7a7e8b7910
Zendy <nguyenduc.051194@gmail.com>
2021-09-25 17:20:12 +0700

    Signup page and transition animation

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -1,11 +1,19 @@
 import 'package:auth_animation/constants.dart';
 import 'package:auth_animation/widgets/login_form.dart';
+import 'package:auth_animation/widgets/signup_form.dart';
 import 'package:auth_animation/widgets/social_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool _isShowSignUp = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,34 +23,64 @@ class AuthScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // Lets animate the transition
           // Login
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             // I use 88% width for login
             width: _size.width * 0.88, // 88%
             height: _size.height,
-            child: Container(
-              color: login_bg,
-              child: LoginForm(),
-            ),
-          ),
-          Positioned(
-            top: _size.height * 0.1,
-            left: 0,
-            right: _size.width * 0.06,
-            child: CircleAvatar(
-              radius: 25,
-              backgroundColor: Colors.white60,
-              child: SvgPicture.asset(
-                "assets/images/animation_logo.svg",
+            left: _isShowSignUp ? -_size.width * 0.76 : 0,
+            child: GestureDetector(
+              onTap: () {
+                // Just use 88% width for login
+                setState(() {
+                  _isShowSignUp = !_isShowSignUp;
+                });
+              },
+              child: Container(
                 color: login_bg,
+                child: LoginForm(),
               ),
             ),
           ),
+
+          // Sign up
+          AnimatedPositioned(
+            duration: defaultDuration,
+            width: _size.width * 0.88,
+            height: _size.height,
+            left: _isShowSignUp ? _size.width * 0.12 : _size.width * 0.88,
+            child: Container(
+              color: signup_bg,
+              child: SignUpForm(),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: defaultDuration,
+            top: _size.height * 0.1,
+            left: 0,
+            right: _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.white60,
+              child: _isShowSignUp
+                  ? SvgPicture.asset(
+                      "assets/images/animation_logo.svg",
+                      color: signup_bg,
+                    )
+                  : SvgPicture.asset(
+                      "assets/images/animation_logo.svg",
+                      color: login_bg,
+                    ),
+            ),
+          ),
           // I shift it 6% to eight because our this sign up bar take 12% width
-          Positioned(
+          AnimatedPositioned(
+            duration: defaultDuration,
             width: _size.width,
             bottom: _size.height * 0.1,
-            right: _size.width * 0.06,
+            right: _isShowSignUp ? -_size.width * 0.06 : _size.width * 0.06,
             child: SocalButtons(),
           ),
         ],
---
+++ lib/widgets/signup_form.dart
@@ -0,0 +1,46 @@
+import 'package:auth_animation/constants.dart';
+import 'package:flutter/material.dart';
+
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.13),
+      child: Form(
+        child: Column(
+          children: [
+            Spacer(),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Email",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: defpaultPadding,
+              ),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                ),
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Confirm Password",
+              ),
+            ),
+            Spacer(flex: 2)
+          ],
+        ),
+      ),
+    );
+  }
+}
